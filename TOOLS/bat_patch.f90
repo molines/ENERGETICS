019c2d003744cc74642ce87234675c63d87d878f
Jean-Marc Molines <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2019-07-19 23:31:37 +0200

    modification of TOOLS/bat_patch.f90 for time

--- TOOLS/bat_patch.f90
+++ TOOLS/bat_patch.f90
@@ -25,9 +25,10 @@ PROGRAM bat_patch
   !!----------------------------------------------------------------------
   IMPLICIT NONE
 
+  INTEGER(KIND=4)  :: jt
   INTEGER(KIND=4)  :: narg, iargc,ijarg
-  INTEGER(KIND=4)  :: npiglo, npjglo
-  INTEGER(KIND=4)  :: npipat, npjpat
+  INTEGER(KIND=4)  :: npiglo, npjglo, npt, ndims
+  INTEGER(KIND=4)  :: npipat, npjpat, nptpat
   INTEGER(KIND=4)  :: ncidb, ierr, id, ncidp, idbat
   INTEGER(KIND=4)  :: iimin, iimax, ijmin, ijmax
 
@@ -39,6 +40,7 @@ PROGRAM bat_patch
   CHARACTER(LEN=80) :: cv_bathy
   CHARACTER(LEN=80) :: cf_copy    
   CHARACTER(LEN=80) :: cdum
+  LOGICAL           :: ll_time=.TRUE.
 
   !!----------------------------------------------------------------------
   narg=iargc()
@@ -48,6 +50,7 @@ PROGRAM bat_patch
      PRINT *
      PRINT *,'   PURPOSE:'
      PRINT *,'     Patch a sub area of the bathy domain with a patch file.'
+     PRINT *,'     This program have been extended to work with any file, not only bathy.'
      PRINT *
      PRINT *,'   ARGUMENTS:'
      PRINT *,'     -b BATHY-file : original bathy file to patch. A working copy'
@@ -85,28 +88,58 @@ PROGRAM bat_patch
   ierr = NF90_OPEN(cf_bathy,NF90_WRITE,ncidb)
   ierr = NF90_INQ_DIMID(ncidb,'x', id ) ; ierr = NF90_INQUIRE_DIMENSION(ncidb,id,len=npiglo )
   ierr = NF90_INQ_DIMID(ncidb,'y', id ) ; ierr = NF90_INQUIRE_DIMENSION(ncidb,id,len=npjglo )
+  ierr = NF90_INQ_DIMID(ncidb,'time_counter',id) 
+  IF ( ierr == NF90_NOERR ) THEN
+     ierr = NF90_INQUIRE_DIMENSION(ncidb,id,len=npt )
+  ELSE
+     ll_time = .FALSE.
+     npt=1
+     PRINT *,'no time to work with !'
+  ENDIF
+  ierr = NF90_INQ_VARID(ncidb, cv_bathy,idbat)
+  ierr = NF90_INQUIRE_VARIABLE(ncidb,idbat,ndims=ndims)
+  IF ( ndims /= 3 ) ll_time = .FALSE.
 
+  PRINT *, 'NDIMS  = ', ndims
   PRINT *, 'NPIGLO = ', npiglo
   PRINT *, 'NPJGLO = ', npjglo
+  PRINT *, 'NPT    = ', npt,  ' LL_TIME : ', ll_time
   ALLOCATE( zbathy(npiglo, npjglo) )
-  ierr = NF90_INQ_VARID(ncidb, cv_bathy,idbat)
-  ierr = NF90_GET_VAR(ncidb,idbat,zbathy)
 
-  ! Now Read patch
+  ! now with the patch
   ierr = NF90_OPEN(cf_patch,NF90_NOWRITE,ncidp)
   ierr = NF90_INQ_DIMID(ncidp,'x', id ) ; ierr = NF90_INQUIRE_DIMENSION(ncidp,id,len=npipat )
         IF ( npipat /= iimax -iimin + 1 ) STOP 'I-Patch size does not fit'
   ierr = NF90_INQ_DIMID(ncidp,'y', id ) ; ierr = NF90_INQUIRE_DIMENSION(ncidp,id,len=npjpat )
         IF ( npjpat /= ijmax -ijmin + 1 ) STOP 'J-Patch size does not fit'
 
+  ierr = NF90_INQ_DIMID(ncidp,'time_counter',id) 
+  IF ( ierr == NF90_NOERR ) THEN
+     ierr = NF90_INQUIRE_DIMENSION(ncidp,id,len=nptpat )
+     IF ( nptpat /= npt ) STOP ' inconstency in time dimension'
+  ENDIF
+
   ALLOCATE( zpatch(npipat, npjpat) )
   ierr = NF90_INQ_VARID(ncidp, cv_patch,id)
-  ierr = NF90_GET_VAR(ncidp,id,zpatch)
-  ierr = NF90_CLOSE(ncidp)  ! no more things to do with this file
 
+
+  DO jt = 1, npt
+  IF ( ll_time ) THEN
+     ierr = NF90_GET_VAR(ncidb,idbat,zbathy(:,:), start=(/1,1,jt/), count=(/npiglo,npjglo,1/) )
+     ierr = NF90_GET_VAR(ncidp,id,   zpatch(:,:), start=(/1,1,jt/), count=(/npipat,npjpat,1/) )
+  ELSE
+     ierr = NF90_GET_VAR(ncidb,idbat,zbathy)
+     ierr = NF90_GET_VAR(ncidp,id,   zpatch)
+  ENDIF
   zbathy(iimin:iimax,ijmin:ijmax) = zpatch
 
-  ierr = NF90_PUT_VAR(ncidb,idbat, zbathy)
+  IF ( ll_time ) THEN
+      ierr = NF90_PUT_VAR(ncidb,idbat,zbathy(:,:), start=(/1,1,jt/), count=(/npiglo,npjglo,1/) )
+  ELSE
+      ierr = NF90_PUT_VAR(ncidb,idbat, zbathy)
+  ENDIF
+  ENDDO
+  ierr = NF90_CLOSE(ncidp)
   ierr = NF90_CLOSE(ncidb)
   
 END PROGRAM bat_patch
