48114599ab4f8f93bd1c3a7fa3f7cf81fa2fadb9
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2019-08-11 14:55:56 +0200

    small modif in nemogcm avoiding use of cform_err

--- DCM_ENERGETICS/DCMTOOLS/DRAKKAR/NEMO4/src/OCE/nemogcm.F90
+++ DCM_ENERGETICS/DCMTOOLS/DRAKKAR/NEMO4/src/OCE/nemogcm.F90
@@ -263,8 +263,8 @@ CONTAINS
       IF ( lwp ) THEN 
       IF( nstop /= 0  ) THEN   ! error print
          WRITE(numout,cform_ccc)   ! Flag ;);););)
-         WRITE(numout,cform_err)
-         WRITE(numout,*) nstop, ' error(s) have been found.' 
+         WRITE(ctmp1,*) '   ==>>>   nemo_gcm: a total of ', nstop, ' errors have been found'
+         CALL ctl_stop( ctmp1 )
       ELSE
          WRITE(numout,cform_aaa)   ! Flag AAAAAAA
       ENDIF
