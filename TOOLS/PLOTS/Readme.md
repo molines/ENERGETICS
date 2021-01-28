93e785ef3ae784237e904e0c9f0c63720118e47e
jmm <Jean-Marc.Molines@univ-grenoble-alpes.fr>
2021-01-28 22:00:03 +0100

    add bash script for python_plot

---
+++ TOOLS/PLOTS/Readme.md
@@ -0,0 +1,1 @@
+ Plot tools are just bash scripts based on python_plot developed in eORCA12.L75-GJM2020/TOOLS/ANALYSES
---
+++ TOOLS/PLOTS/vitmod_plot.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --ntasks=40
+#SBATCH --ntasks-per-node=40
+#SBATCH --threads-per-core=1
+#SBATCH --hint=nomultithread
+#SBATCH -A cli@cpu
+#SBATCH -J JOB_
+#SBATCH -e zjob.e%j
+#SBATCH -o zjob.o%j
+#SBATCH --time=2:30:00
+#SBATCH --exclusive
+
+set -x
+
+vp="-100 -10 20 70"
+zoom="1 1  5014 3745"
+figs=./fig_vitmod
+var=sovitmod
+pal=RdBu_r
+charpal=nrl
+proj=merc     # merc cyl 
+bckgrd=shadedrelief   # none etopo shadedrelief bluemarble
+vmax=-9999
+vmin=-9999
+width=7   # Plot frame in inches
+height=6
+res=i     # resolution of the coast line c l i h f 
+klev=-1
+dep=10
+depv="deptht"
+
+
+xstep=30
+ystep=15
+
+y1=2006
+y2=2006
+
+mkdir -p $figs
+
+nmax=1
+n=0
+for y in $(seq $y1 $y2 ) ; do
+
+for f in eNATL36X-ENERGETICS_y2006m03d15h00_vita.nc ; do
+   ff=$(basename $f )
+   g=${ff%.nc} 
+   if [ ! -f $figs/$g.png ] ; then
+     ( 
+      python_plot.py -i $g -v $var  -p $pal -proj $proj -xstep $xstep -ystep $ystep \
+            -wij $zoom -wlonlat $vp  -d $figs -bckgrd $bckgrd -vmax $vmax -vmin $vmin \
+            -figsz $width $height -res $res 
+       ) &
+      
+    n=$(( n + 1 ))
+    if [ $n = $nmax ] ; then
+          wait
+          n=0
+    fi
+   else 
+     echo $g.png already done
+   fi
+done
+done
+wait
